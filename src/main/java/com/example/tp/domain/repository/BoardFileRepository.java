d97623c12eed4abe8ad3a06a1d6388c870fe8817
honghyelim <hyelim8112@naver.com>
2023-10-09 14:53:22 +0900

    수정

--- src/main/java/com/example/tp/domain/repository/BoardFileRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.tp.domain.repository;
-
-
-import com.example.tp.domain.entity.BoardFileEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardFileRepository extends JpaRepository<BoardFileEntity, Long> {
-}
